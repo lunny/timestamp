dd3c7b7b3e0789dbbb92c0f903702db81e8e34b7
Lunny Xiao <xiaolunwen@gmail.com>
2018-12-18 10:47:12 +0800

    init project

---
+++ timestamp_test.go
@@ -0,0 +1,19 @@
+// Copyright 2018 Lunny Xiao. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package timestamp
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimeStamp(t *testing.T) {
+	ti := time.Now()
+	tm := TimeStamp(ti.Unix())
+	assert.EqualValues(t, ti.Unix(), tm)
+	assert.EqualValues(t, ti.Format("2006-01-02 15:04:05"), tm.Format("2006-01-02 15:04:05"))
+}
